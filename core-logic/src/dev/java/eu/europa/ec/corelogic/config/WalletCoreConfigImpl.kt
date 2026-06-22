7f121f94a35172c6a403dac8ff6f1dfc1ab34176
Lassi Palojärvi <lassi.palojarvi@outlook.com>
2026-06-22 20:24:52 +0300

    Add Multipaz reader trust anchor

--- core-logic/src/dev/java/eu/europa/ec/corelogic/config/WalletCoreConfigImpl.kt
+++ core-logic/src/dev/java/eu/europa/ec/corelogic/config/WalletCoreConfigImpl.kt
@@ -153,7 +153,8 @@ internal class WalletCoreConfigImpl(
                         R.raw.pidissuerca02_pt,
                         R.raw.pidissuerca02_ut,
                         R.raw.dc4eu,
-                        R.raw.r45_staging
+                        R.raw.r45_staging,
+                        R.raw.multipaz
                     )
 
                     if (dynamicCerts.isNotEmpty()) {
